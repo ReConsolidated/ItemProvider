35eb75f169593da7283d66fcfeafbc52033f0342
ReConsolidated <gracjanpasik@gmail.com>
2022-04-11 01:34:49 +0200

    Updates

--- src/main/java/io/github/reconsolidated/itemprovider/ItemProviderCommand.java
+++ src/main/java/io/github/reconsolidated/itemprovider/ItemProviderCommand.java
@@ -8,6 +8,8 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.UUID;
+
 public class ItemProviderCommand implements CommandExecutor {
     private final ItemProvider itemProvider;
 
@@ -22,20 +24,38 @@ public class ItemProviderCommand implements CommandExecutor {
             return true;
         }
         if (args[0].equalsIgnoreCase("add")) {
-            if (args.length < 3) {
-                sender.sendMessage(ChatColor.RED + "Poprawne użycie: ");
-                sender.sendMessage(ChatColor.AQUA + "/itemprovider add <name> <category> - dodaje item z ręki");
+            if (args.length < 2) {
+                sender.sendMessage(ChatColor.RED + "Correct usage: ");
+                sender.sendMessage(ChatColor.AQUA + "/itemprovider add <category> [name]  - adds item from hand");
             } else {
                 if (sender instanceof Player) {
                     Player player = (Player) sender;
                     if (player.getInventory().getItemInMainHand() == null
                             || player.getInventory().getItemInMainHand().getType().equals(Material.AIR)) {
-                        sender.sendMessage(ChatColor.RED + "Musisz trzymać item w ręce!");
+                        sender.sendMessage(ChatColor.RED + "You have to hold the item in hand!");
                     } else {
-                        String name = args[1];
-                        String category = args[2];
+                        String name = UUID.randomUUID().toString();
+                        if (args.length >= 3) {
+                            name = args[2];
+                        }
+                        String category = args[1];
                         itemProvider.addItem(player.getInventory().getItemInMainHand(), name, category);
+                        sender.sendMessage(ChatColor.GREEN + "Item added!");
                     }
+                }
+            }
+        }
+        if (args[0].equalsIgnoreCase("get")) {
+            if (args.length < 3) {
+                sender.sendMessage(ChatColor.RED + "Correct usage: ");
+                sender.sendMessage(ChatColor.AQUA + "/itemprovider get <category> <name>  - gives you item");
+            } else {
+                if (sender instanceof Player) {
+                    Player player = (Player) sender;
+                    String name = args[2];
+                    String category = args[1];
+                    player.getInventory().addItem(itemProvider.getItem(category, name));
+                    sender.sendMessage(ChatColor.GREEN + "Item given!");
                 }
             }
         }
@@ -44,6 +64,7 @@ public class ItemProviderCommand implements CommandExecutor {
 
     private void onHelp(CommandSender sender) {
         sender.sendMessage(ChatColor.AQUA + "ItemProvider help: ");
-        sender.sendMessage(ChatColor.AQUA + "/itemprovider add <name> <category> - dodaje item z ręki");
+        sender.sendMessage(ChatColor.AQUA + "/itemprovider add <category> [name] - dodaje item z ręki");
+        sender.sendMessage(ChatColor.AQUA + "/itemprovider get <category> <name> - dodaje item z ręki");
     }
 }
